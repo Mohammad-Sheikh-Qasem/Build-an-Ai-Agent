483aefc8b0e6e67fb178e465614e11abb65e40ca
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-06-29 18:38:53 +0300

    fix bug: there is a comma that was forgot

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents=messages
+    contents=messages,
     config=types.GenerateContentConfig(
         system_instruction=system_prompt
     ),
