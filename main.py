694b1f8e2e4ed533f2290fbe08a67d6c74231f8e
Mohammad Sheikh Qasem <mohamad.sheikh.qasem.cs.e@gmail.com>
2026-06-30 11:47:47 +0300

    update main.py

--- main.py
+++ main.py
@@ -28,7 +28,8 @@ response = client.models.generate_content(
     model="gemini-2.5-flash",
     contents=messages,
     config=types.GenerateContentConfig(
-        system_instruction=system_prompt
+        system_instruction=system_prompt,
+        temperature=0,
     ),
 )
 
